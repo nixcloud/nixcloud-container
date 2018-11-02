fd95f6b53a44dc76b6cbbed5c0299b9b900ed72c
Paul Seitz <paul.m.seitz@gmail.com>
2018-11-02 10:47:10 +0000

    bugfix: proper version testing

--- bin/helper/patches.nix
+++ bin/helper/patches.nix
@@ -3,7 +3,7 @@ let
     lib = super.stdenv.lib;
 in
 {
-  systemd = if ((lib.toInt super.systemd.version) < 239) then
+  systemd = if ((builtins.compareVersions super.systemd.version "239") == -1) then
     super.systemd.overrideAttrs (drv: {
     # https://github.com/lxc/lxc/issues/2226
     patches = if super.systemd ? patches then super.systemd.patches ++ [ ./8447.patch ]
